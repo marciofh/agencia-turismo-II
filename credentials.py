38c6711f715e5236c8976e09c2913d8eea82bbde
Marcio Filho <marciof1555@gmail.com>
2022-12-08 12:29:27 -0300

    credenciais

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
 keys = {
-    'token': "5806a2bf91msh9ab0271eb591604p15f5dajsnfbff5dd4ba5e",
-    'HOST': "localhost",
-    'DBNAME': "turismo",
-    'USER': "postgres",
-    'PASSWORD': "senha"
+    'token': "",
+    'HOST': "",
+    'DBNAME': "",
+    'USER': "",
+    'PASSWORD': ""
 }
